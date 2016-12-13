77cbad9e153993142a17a64cbcbc6b28ac61c272
Tanya Zamorska <zamorskatetiana@gmail.com>
2016-12-13 15:11:53 +0200

    add 'remove' button

--- public_html/main.js
+++ public_html/main.js
@@ -5,8 +5,11 @@ addButton.onclick = function() {
   var input = document.querySelectorAll('input')[0];
   var inputValue = input.value;
   var li = document.createElement('li');
-  li.innerText = inputValue;
+  li.innerText = inputValue + ' ';
   var ul = document.querySelectorAll('ul')[0];
   ul.appendChild(li);
   input.value = " ";
+  var removeButton = document.createElement('button');
+  removeButton.innerText = "remove";
+  li.appendChild(removeButton);
 };
