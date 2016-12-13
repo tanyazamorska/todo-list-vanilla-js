fc77754112894866b1c954d8802fb20bf3872310
Tanya Zamorska <zamorskatetiana@gmail.com>
2016-12-13 13:15:37 +0200

    'add' button add new todo to todolist when clicked

--- public_html/main.js
+++ public_html/main.js
@@ -1,4 +1,12 @@
 "use strict";
 
-
-
+var addButton = document.querySelectorAll('button')[0];
+addButton.onclick = function() {
+  var input = document.querySelectorAll('input')[0];
+  var inputValue = input.value;
+  var li = document.createElement('li');
+  li.innerText = inputValue;
+  var ul = document.querySelectorAll('ul')[0];
+  ul.appendChild(li);
+  input.value = " ";
+};
